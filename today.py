541c8ababdb8fc065e77fe7b5a087a7f52f595c4
Gokce Yuksel <gokceyuksel27@gmail.com>
2025-12-22 00:18:31 +0300

    Fix style issues, should get a Good Style

--- today.py
+++ today.py
@@ -1,4 +1,9 @@
+"""A module computing buddy pair names for the day"""
+
 def my_name_is():
+    """Return my GitHub username."""
     return "gokce-yuksel"
+
 def my_buddy_is():
+    """Return my buddy's GitHub username."""
     return "gokce-yuksel"
